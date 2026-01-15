c36818c0b0c3be45efd101552bf899671a270c8b
Paulo <paulocavalcante879@gmail.com>
2026-01-15 20:00:30 -0300

    First commit

---
+++ scripts.js
@@ -0,0 +1,51 @@
+// Visualizar o conteúdo do document
+// console.log(document)
+
+//Obter title da página
+console.log(document.title)
+
+const guest = document.getElementById("guest-2")
+
+console.log(guest)
+
+//Mostra as propriedades do objeto
+
+//console.dir(guest)
+
+const guestsByClass = document.getElementsByClassName("guest")
+
+console.log(guestsByClass)
+
+console.log(guestsByClass.item(0))
+console.log(guestsByClass[1])
+
+const guestByTag = document.getElementsByTagName("li")
+console.log(guestByTag)
+
+console.log(document.querySelector("#guest-2"))
+console.log(document.querySelectorAll(".guest"))
+
+const guest2 = document.querySelector("#guest-2 span")
+
+guest2.textContent = "Maria"
+
+console.log(guest2.textContent) // Retorna o conteúdo visivel e oculto
+console.log(guest2.innerText) // Retorna só o conteu visivel
+console.log(guest2.innerHTML) // Retorna o HTML
+
+
+const input = document.querySelector("#name")
+
+input.classList.add("input-error")
+
+input.classList.remove("input-error")
+
+input.classList.toggle("input-error") // se tiver remove se não tiver adiciona
+
+const button = document.querySelector("button")
+
+button.style.backgroundColor = "red"
+
+const guests = document.querySelector("ul")
+
+const newGuest = document.createElement()
